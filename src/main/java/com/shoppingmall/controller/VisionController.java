59ef79491acdf5b48047585bbf8d486c9523e569
nayoung188 <naya1649@naver.com>
2023-08-09 17:57:29 +0900

    css 보강가즈아ㅏㅏ

--- src/main/java/com/shoppingmall/controller/VisionController.java
+++ src/main/java/com/shoppingmall/controller/VisionController.java
@@ -11,10 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
